44b5cd2a450fe172d6ae94fc5f4885293627198f
Campbell Moss <campbell.moss@vacasa.com>
2023-01-03 21:05:19 +1300

    Make patrol log serializable

--- lib/src/models/patrol_log.dart
+++ lib/src/models/patrol_log.dart
@@ -1,6 +1,8 @@
 import 'package:cpnz/src/models/patrol_incident.dart';
 import 'package:cpnz/src/models/route_point.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+@JsonSerializable()
 class PatrolLog {
   final List<RoutePoint> route = [];
   final List<PatrolIncident> incidents = [];
